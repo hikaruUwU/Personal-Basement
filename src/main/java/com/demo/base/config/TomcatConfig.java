ab0e0cea9c8a0a4ee586ab94aab60536fa872557
UwU <uwu.fionia@gmail.com>
2025-11-09 15:39:05 +0800

    latest:latest

--- src/main/java/com/demo/base/config/TomcatConfig.java
+++ src/main/java/com/demo/base/config/TomcatConfig.java
@@ -10,17 +10,7 @@ import java.util.concurrent.*;
 
 @Configuration
 public class TomcatConfig {
-    private final Executor executor = new ThreadPoolExecutor(
-            Runtime.getRuntime().availableProcessors(),
-            Integer.MAX_VALUE,
-            60L,
-            TimeUnit.SECONDS,
-            new SynchronousQueue<>(),
-            Thread.ofVirtual().name("tomcat-executor",0).factory(),
-            new ThreadPoolExecutor.DiscardPolicy()
-    );
-
-
+    private final Executor executor = Executors.newVirtualThreadPerTaskExecutor();
 
     @Bean
     public ServletWebServerFactory servletContainer() {
